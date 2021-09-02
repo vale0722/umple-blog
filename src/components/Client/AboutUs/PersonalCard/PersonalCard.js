b4107c92565c1049f0c60339860595300434cb51
Valeria Granada Rodas <55161707+vale0722@users.noreply.github.com>
2021-09-01 23:50:39 -0500

    fix: add noreferrer

--- src/components/Client/AboutUs/PersonalCard/PersonalCard.js
+++ src/components/Client/AboutUs/PersonalCard/PersonalCard.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import {CardSection, AboutUsImg, AboutUsImgContainer, CardContainer} from './PersonalCard.elements'
-import {FaGithub} from 'react-icons/fa'
-import {IconContext} from "react-icons";
+import { CardSection, AboutUsImg, AboutUsImgContainer, CardContainer } from './PersonalCard.elements'
+import { FaGithub } from 'react-icons/fa'
+import { IconContext } from "react-icons";
 
-const PersonalCard = ({image, name, description, github}) => {
+const PersonalCard = ({ image, name, description, github }) => {
     return (
 
-        <IconContext.Provider value={{className: "w-full self-center mt-6"}}>
+        <IconContext.Provider value={{ className: "w-full self-center mt-6" }}>
             <CardSection className="bg-white shadow-lg">
                 <AboutUsImgContainer>
                     <AboutUsImg className="rounded-full" src={image}></AboutUsImg>
@@ -14,7 +14,7 @@ const PersonalCard = ({image, name, description, github}) => {
                 <CardContainer className="text-center">
                     <h1 className="text-lg font-bold">{name}</h1>
                     <h2>{description}</h2>
-                    <a className="text-center w-full mt-6" target="_blank" href={github}>
+                    <a className="text-center w-full mt-6" target="_blank" rel="noreferrer" href={github}>
                         <FaGithub></FaGithub>
                     </a>
                 </CardContainer>
