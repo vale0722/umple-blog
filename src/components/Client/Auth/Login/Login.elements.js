60b2434f2339a741afd3d47a89c157115a600119
alejociro <alejandro.castrillonc@udea.edu.co>
2021-09-28 19:09:28 -0500

    feat: add login

--- src/components/Client/Auth/Login/Login.elements.js
+++ src/components/Client/Auth/Login/Login.elements.js
@@ -1,17 +1,35 @@
-import styled from 'styled-components'
-import { Container } from '../../../../globalStyles'
+import styled from "styled-components";
+import { Container } from "../../../../globalStyles";
 
 export const LoginSesion = styled(Container)`
-    height: 100%;
-    width: 100%;
-    margin: 20px 0 !important;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-`
+  height: 100%;
+  width: 100%;
+  margin: 20px 0 !important;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
 
 export const ImageContainer = styled.img`
-    height: 20%;
-    width: 20%;
-`
+  align-items:center
+  margin-top: 10px;
+  height: 190px;
+  width: 180px;
+  margin-left: 32px;
+`;
+
+export const containerSignIn = styled.div`
+  height: 100%;
+  width: 100%;
+`;
+
+export const titleSignIn = styled.h1``;
+
+export const ImageContainer2 = styled.img`
+  margin-top: 10px;
+  height: 400px;
+  width: 872px;
+  margin-left: 280px;
+  position: absolute;
+`;
--- src/components/Client/Auth/Login/Login.js
+++ src/components/Client/Auth/Login/Login.js
@@ -1,17 +1,50 @@
-import React from 'react'
-import { LoginSesion, ImageContainer } from './Login.elements'
-import { Card, Title } from '../../../../globalStyles'
-import account from '../../../../images/svgs/account.svg'
+import React from "react";
+import { LoginSesion, ImageContainer, ImageContainer2 } from "./Login.elements";
+import { Title } from "../../../../globalStyles";
+import account from "../../../../images/svgs/account.svg";
+import sigin from "../../../../images/svgs/signin.svg";
 
 const Login = () => {
-    return (
-        <LoginSesion>
-            <ImageContainer src={account}></ImageContainer>
-            <Title>Inicia Sesión</Title>
-            <Card width="50%">Email</Card>
-            <Card width="50%">Contraseña</Card>
-        </LoginSesion>
-    )
-}
+  return (
+    <div className="flex-row">
+      <div className="bg-opacity-50 bg-black h-full flex items-center justify-center w-full"></div>
+      <ImageContainer2 src={sigin}></ImageContainer2>
 
-export default Login
+      <div className="mt-28 w-80 h-120 border-solid rounded-sm border-color:#00BCD4">
+        <ImageContainer src={account}></ImageContainer>
+        <div className="m-3.5">
+          <h1 className="w-44 text-2xl mx-8">Iniciar Sesion</h1>
+        </div>
+        <input
+          type="text"
+          name="comment"
+          id="comment"
+          placeholder="Correo Electronico"
+          className="w-min text-sm py-4 px-3 rounded-xl focus:shadow-md hover:shadow-md focus:outline-none m-2 lind"
+        />
+        <input
+          type="text"
+          name="comment"
+          id="comment"
+          placeholder="Contraseña"
+          className="w-min text-sm py-4 px-3 rounded-xl focus:shadow-md hover:shadow-md focus:outline-none m-2"
+        />
+        <div class="w-full min-h-screen flex justify-center items-center">
+          <button
+            data-modal-toggle="example2"
+            data-modal-action="open"
+            class="bg-indigo-600 font-semibold text-white p-2 w-32 rounded-full hover:bg-purple-700 focus:outline-none focus:ring shadow-lg hover:shadow-none transition-all duration-300 m-2 mr-24"
+          >
+            Iniciar sesion
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
+
+/*
+
+    */
